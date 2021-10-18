29e6d57934994e883957f6c27c532086a1bc1246
高伟 <gaowei@gaoweideMBP.lan>
2021-10-18 22:17:38 +0800

    opt: 完善用户名

--- client.go
+++ client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -11,6 +13,7 @@ type Client struct {
 	ServerPort int
 	Name string
 	conn net.Conn
+	flag int // 当前用户模式
 }
 
 func NewClient(ip string, port int) *Client {
@@ -18,6 +21,7 @@ func NewClient(ip string, port int) *Client {
 	client := &Client {
 		ServerIp: ip,
 		ServerPort: port,
+		flag: 999,
 	}
 
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", ip, port))
@@ -29,6 +33,65 @@ func NewClient(ip string, port int) *Client {
 	client.conn = conn
 
 	return client
+}
+
+func (client *Client) menu() bool {
+	var flag int
+
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.修改用户名")
+	fmt.Println("0.退出")
+
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println("请输入正确的数值")
+		return false
+	}
+}
+
+func (client *Client) updateName() bool {
+	fmt.Println("请输入用户名：")
+	fmt.Scanln(&client.Name)
+
+	sendMsg := "rename|" + client.Name + "\n"
+
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("write msg err:", err)
+		return false
+	}
+
+	return true
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.menu() != true {
+
+		}
+
+		switch client.flag {
+		case 1:
+			fmt.Println("开启公聊模式")
+			break
+		case 2:
+			fmt.Println("开启私聊模式")
+			break
+		case 3:
+			client.updateName()
+			break
+		}
+	}
+}
+
+// 阻塞监听服务端响应，并输出至命令行标准输出
+func (client *Client) DealResponse() {
+	io.Copy(os.Stdout, client.conn)
 }
 
 var serverIp string
@@ -52,5 +115,7 @@ func main() {
 
 	fmt.Println("创建client成功")
 
-	select{}
+	go client.DealResponse()
+
+	client.Run()
 }
